be41f1bb16b9bf0ce35fec8b8efe707c2df12647
tim97302 <67775335+tim97302@users.noreply.github.com>
2020-08-14 11:55:33 +0800

    2020/08/14

---
+++ autolane.py
@@ -0,0 +1,42 @@
+import cv2
+import autolane_module as m
+import numpy as np
+import turn_signal as turn
+
+def lane(img,a,b,c,d):
+    edge = m.get_edge(img)                  # 邊緣偵測
+    roi = m.get_roi(edge)                   # 取得 ROI
+    left_brightness=m.get_brightness_left(img,a,b,c,d)      # 取得左車燈亮度
+    text="cross lane"
+    print(left_brightness)
+    # =========做判斷亮度，若有在圖上加工標示=======
+    lines = cv2.HoughLinesP(image=roi,      # Hough 轉換
+                            rho=3,
+                            theta=np.pi/180,
+                            threshold=30,
+                            minLineLength=20,
+                            maxLineGap=50)
+    # avglines = m.get_avglines(lines)          # 取得左右 2 條平均線方程式
+    # if avglines is not None:
+    #     lines = m.get_sublines(img, avglines,a,c,d)  # 取得要畫出的左右 2 條線段，並以check_point判斷是否有切車道
+    #     img = m.draw_lines(img, lines[0])  # 畫出線段
+    #     ans=lines[1]
+    #     p=0
+    #     if ans==1:
+    #         p=1
+    #     if ans==2:
+    #         p=0
+    #     if (ans==0 and p==1) or (ans==1 and p==1):
+    #         cv2.putText(img, text, (10, 80), cv2.FONT_HERSHEY_PLAIN,3, (255, 0, 0), 3, cv2.LINE_AA)
+        # cv2.putText(影像, 文字,  座標,            字型,        大小, 顏色,    線條寬度, 線條種類)
+        # print(img.shape)
+        # cv2.imshow('Frame', img)
+
+    cv2.imshow('Frame', img)  # 顯示影像
+    # return img
+    k = cv2.waitKey(1)  # 等待按鍵輸入
+    if k == ord('q') or k == ord('Q'):  # 按下 Q(q) 結束迴圈
+        print('exit')
+        cv2.destroyAllWindows()  # 關閉視窗
+
+
