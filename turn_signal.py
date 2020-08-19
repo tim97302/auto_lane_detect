58337ae2c5d66c7106ab629668bf04115d724f8a
tim97302 <67775335+tim97302@users.noreply.github.com>
2020-08-19 09:57:40 +0800

    0819

--- turn_signal.py
+++
@@ -1,8 +0,0 @@
-import cv2
-from PIL import Image
-from PIL import ImageStat
-def turn_signal(input):
-    im = Image.fromarray(cv2.cvtColor(input, cv2.COLOR_BGR2RGB))
-    im = im.convert('L')
-    stat = ImageStat.Stat(im)
-    print(stat.mean[0])
